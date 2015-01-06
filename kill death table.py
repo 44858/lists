d0fe8eb4e9d9fd2ae2fe11f38fedb6223b53657a
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2015-01-06 13:03:58 +0000

    kill/ death table

---
+++ kill death table.py
@@ -0,0 +1,14 @@
+#Lewis Travers
+#06/01/2015
+#kill/death table
+
+players =[
+    ["k1llmAchine", 51, 49],
+    ["bob2247", 5, 99],
+    ["hAxOr12", 72, 30]
+]
+print("| Name | Kills | Deaths |\n| {0} | {1} | {2} |\n| {3} | {4} | {5} |\n| {6} | {7} | {8} |".format(players[0][0], players[0][1], players[0][2], players[1][0], players[1][1], players[1][2], players[2][0], players[2][1], players[2][2]))
+
+def table():
+    
+    
