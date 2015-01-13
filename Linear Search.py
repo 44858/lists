b9c65a9496d5c72cde6ded40f69f23094447ff7e
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2015-01-13 13:00:43 +0000

    searching in a list

--- Linear Search.py
+++ Linear Search.py
@@ -5,19 +5,19 @@
 search_list = ["person 1", "person 2","person 3", "person 4", "person 5", "person 6"]
 
 def details():
-    search_item = input("Please enter the item that you would like to search for")
+    search_item = input("Please enter the item that you would like to search for: ")
     return search_item
 
 def linear_search(search_list, search_item):
     found = False
     count = 0
-    while not found and count < len(search_list):
+    while found == False and count < len(search_list):
         if search_list[count] == search_item:
             print("Found")
             found = True
         else:
             print("Not found")
-    count = count + 1
+        count = count + 1
     return found
 
 # main program
